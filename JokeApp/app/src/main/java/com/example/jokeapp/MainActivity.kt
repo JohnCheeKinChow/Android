167461a085dccb4ce40d9face5124e6c8e84683b
JohnCheeKinChow <Joepie234567890>
2021-10-12 10:08:45 +0200

    [NEW] Databinding toegevoegd

--- JokeApp/app/src/main/java/com/example/jokeapp/MainActivity.kt
+++ JokeApp/app/src/main/java/com/example/jokeapp/MainActivity.kt
@@ -7,27 +7,51 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.jokeapp.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var jokeTextView: TextView
-    lateinit var SmileyImage: ImageView
+    /*lateinit var jokeTextView: TextView
+    lateinit var SmileyImage: ImageView*/
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setClickListeners()
+
 
         var nextJokeButton: Button = findViewById(R.id.nextJoke_button)
         var nextSmileyButton: Button = findViewById(R.id.nextSmile_button)
 
-        jokeTextView = findViewById(R.id.joke_textview)
+        //jokeTextView = findViewById(R.id.joke_textview)
         //nextJokeButton.text = "Different Text"
-        nextJokeButton.setOnClickListener {
+
+        /*binding.nextJokeButton.setOnClickListener {
             showNextButton()
         }
-        nextSmileyButton.setOnClickListener {
+        binding.nextSmileButton.setOnClickListener {
             showNextSmiley()
+        }*/
+    }
+
+    private fun setClickListeners() {
+        val clickableObjects = listOf(binding.nextJokeButton, binding.nextSmileButton)
+
+        for (item in clickableObjects) {
+            when (item.id) {
+                R.id.nextSmile_button -> item.setOnClickListener {
+                    showNextSmiley()
+                }
+                R.id.nextJoke_button -> item.setOnClickListener {
+                    showNextButton()
+                }
+            }
         }
     }
 
@@ -42,13 +66,15 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.ic_iconmonstr_smiley_2
         }
 
-        SmileyImage.setImageResource(source)
+        binding.smileyImageview.setImageResource(source)
     }
 
     private fun showNextButton() {
         Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
         //var jokeTextView: TextView = findViewById(R.id.joke_textview)
         var myJokeBook: jokebook = jokebook()
-        jokeTextView.text = myJokeBook.getRandomJoke()
+        binding.jokeTextview.text = myJokeBook.getRandomJoke()
     }
+
+
 }
